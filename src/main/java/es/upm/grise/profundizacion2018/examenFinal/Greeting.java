8c5eaf22b76bea9c1bd48a0089044485e1a92149
DanielRabasco <danielrabascogarcia@gmail.com>
2019-01-21 15:38:00 +0100

    MyCalendar class implemented

--- src/main/java/es/upm/grise/profundizacion2018/examenFinal/Greeting.java
+++ src/main/java/es/upm/grise/profundizacion2018/examenFinal/Greeting.java
@@ -1,29 +1,16 @@
 package es.upm.grise.profundizacion2018.examenFinal;
 
-import java.util.Calendar;
-
 public class Greeting {
 	
+	private MyCalendar myCalendar;
+	
+	public Greeting () {
+		myCalendar = new MyCalendar();
+	}
 	public String getGreeting( Language language ) {
-		// Get current hour
-		Calendar calendar = Calendar.getInstance();
-		int hour = calendar.get( Calendar.HOUR_OF_DAY );
-		
-		// Find out the greeting language
-		if ( language == null )
-			language = Message.getDefaultLanguage();
-		
-		// Get the moment of the day
-		TimeOfTheDay moment;
-		if( hour < 12 ) 
-			moment = TimeOfTheDay.MORNING;
-		else if ( hour < 18 )
-			moment = TimeOfTheDay.AFTERNOON;
-		else
-			moment = TimeOfTheDay.EVENING;
-			
-		// Return the message		
-		return Message.getMessage( moment, language );
+		myCalendar.SetLanguage(language);
+		myCalendar.SetMoment();
+		return myCalendar.GetGreeting();
 	}
 
 }
---
+++ src/main/java/es/upm/grise/profundizacion2018/examenFinal/MyCalendar.java
@@ -0,0 +1,38 @@
+package es.upm.grise.profundizacion2018.examenFinal;
+
+import java.util.Calendar;
+
+public class MyCalendar {
+	
+	public String greeting;
+	private Calendar calendar;
+	private int hour;
+	private TimeOfTheDay moment;
+	private Language language;
+	
+	public MyCalendar () {
+		calendar = Calendar.getInstance();
+		hour = calendar.get( Calendar.HOUR_OF_DAY );
+	}	
+
+	public String GetGreeting(){								
+		// Return the message		
+		return Message.getMessage( moment, language );
+	}
+	
+	public void SetLanguage(Language language) {
+		// Find out the greeting language
+		if ( language == null )
+			this.language = Message.getDefaultLanguage();
+	}
+	
+	public void SetMoment() {
+		// Get the moment of the day
+		if( hour < 12 ) 
+			moment = TimeOfTheDay.MORNING;
+		else if ( hour < 18 )
+			moment = TimeOfTheDay.AFTERNOON;
+		else
+			moment = TimeOfTheDay.EVENING;			
+	}
+}
